711042e6981b56a50361aa1dd5a606d3bd65bb36
Jose9734 <JOLEON@augusta.edu>
2025-12-03 21:35:33 -0500

    3

--- 02-label/label.py
+++ 02-label/label.py
@@ -1,14 +1,15 @@
 # Prompt the user to for a first name
-
+first = input("First name:")
 
 # Prompt the user to for a last name
-
+Last = input("Last name:")
 
 # Prompt the user for a job title
-
+Job = input("Job title:")
 
 # Print the values label in the form:
 #
 # last_name, first_name
 # title
-
+print(Last, first)
+print(Job)
--- 03-einstein/einstein.py
+++ 03-einstein/einstein.py
@@ -1,9 +1,9 @@
 # Prompt the user for mass as an integer (in kilograms)
-
-
+mass = input(int("Enter mass in kilograms:"))
+light = 300,000,000
 # Calculate the Einstein formula (don't forget to convert the input from str to
 # int)
-
+Einstein = mass * light * light
 
 # Print the result
-
+print(f"Value: {Einstein}")
